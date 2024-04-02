df733bb20f4cd4378ef12f366f941026b56c5f1a
Olayiwola Olanrewaju <larry_coal@outlook.com>
2024-04-02 16:07:24 -0400

    template for landing page

---
+++ east-bank-landing-page/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { BgIntroBck, IconApi, IconBudgeting,IconClose,IconFacebook,Logo } from "./assets/index";
+
+function App() {
+  return (
+    <div>
+      <div>Hello</div>
+      {/* <BgIntroBck /> */}
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
